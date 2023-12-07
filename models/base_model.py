52a7903f68bcfb0c66d0ba967ec064a3a8519093
Ndman90 <onwuamaegbund99@gmail.com>
2023-12-07 03:53:17 -0800

    Base Model updated

--- models/base_model.py
+++ models/base_model.py
@@ -4,11 +4,21 @@ from uuid import uuid4
 from datetime import datetime
 
 class BaseModel():
+    """Base class for other classes in the AirBnB clone"""
 
-    def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """Initialize a new instance of the BaseModel"""
+
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
       """Returns official string representation"""
