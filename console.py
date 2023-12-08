7025cb953e10baa8a0f8ace23ee80af89d8ad860
Solomon Bassey <Solomonbassey20@gmail.com>
2023-12-08 08:54:38 +0100

    Included FileStorage class

--- console.py
+++ console.py
@@ -1,18 +1,35 @@
 #!/usr/bin/python3
-
+""" Console module fo AirBnB Clone """
 import cmd
+import json
+import re
+from models.base_model import BaseModel
+from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
+    """Class for the console of AirBnB Clone"""
+
     prompt = "(hbnb) "
+    _all_classes = {
+            "BaseModel": BaseModel
+    }
 
     def do_EOF(self, arg):
+        """EOF (ctrl + d) command to exit the program
+        """
         print()
         return True
 
     def do_quit(self, arg):
+        """Quit command to exit the program
+        """
         return True
     
     def emptyline(self):
+        """An empty line + ENTER command should not
+        execute anything
+        """
         pass
 
 if __name__ == "__main__":
--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,65 @@
 #!/usr/bin/python3
-
+"""BaseModel is a class that encapsulates shared attributes
+and method, serving as a bluprint for other classes. It
+manages the initialization, serialization, and deserial-
+ization of future instances, ensuring consistent their
+consistent handling
+"""
 from uuid import uuid4
 from datetime import datetime
+from models
 
 class BaseModel():
     """Base class for other classes in the AirBnB clone"""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new instance of the BaseModel"""
+        """Constructor for the BaseModel class.
+        Args:
+            *args: Variable number of positional arguments.
+            **kwargs: Keyword arguments.
+        """
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
+                    if key in ["created_at", "updated_at"]:
+                        format = "%Y-%m-%dT%H:%M:%S.%f"
+                        value = datetime.strptime(value, format)
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
-      """Returns official string representation"""
+      """Returns official string representation
 
-      return "[{}] ({}) {}".\
-          format(self.__class__.__name__, self.id, self.__dict__)
+      Returns:
+        str: A string containing the class name, the objects ID,
+        and its attributes.
+      """
+
+      return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
+        """Updates the "updated_at" attribute with the current
+        timestamp.
+
+        Returns:
+            None
+        """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+         """Returns a dictionary representation of the object.
+
+         Returns:
+            dict: A dictionary containing the objects attributes and
+            the "__class__" key with the class name. Datetime objects
+            are converted to ISO formatted strings.
+            """
         dictionary = {}
         for key, value in self.__dict__.items():
             if key == "created_at" or key == "updated_at":
