2bdcd198ebde0c1d4cb61517819a94099bbb1cff
Solomon Bassey <Solomonbassey20@gmail.com>
2023-12-08 11:48:52 +0100

    console.py refactor version1.0

--- console.py
+++ console.py
@@ -32,5 +32,34 @@ class HBNBCommand(cmd.Cmd):
         """
         pass
 
+    def method__error(self, name, arg):
+        """Handler for methods error (helper function)
+        return 0 if no error found otherwise 1
+        """
+        args = arg.split()
+
+        if len(args) < 1 and name != "all":
+            print("** class name missing **")
+            return 1
+
+        classes = HBNBCommand._all_classes
+        if len(args) > 0 and args[0] not in classes:
+            print("** class doesn't exist **")
+            return 1
+
+        if len(args) < 2 and name not in ["create", "all"]:
+            print("** instance id missing **")
+            return 1
+
+        if name not in ["create", "all"]:
+            instance_key = f'{args[0]}.{args[1]}'
+            if instance_key not in storage.all():
+                print("** no instance found **")
+                return 1
+
+        if name == "update":
+            if len(args) < 3:
+                print()
+                return 1
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
